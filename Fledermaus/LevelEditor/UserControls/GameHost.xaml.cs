c3210a8a33f360c822eb7a9e9676a0c8e58a1577
IndyM <123indym@gmail.com>
2016-05-02 17:34:48 +0200

    a

--- Fledermaus/LevelEditor/UserControls/GameHost.xaml.cs
+++ Fledermaus/LevelEditor/UserControls/GameHost.xaml.cs
@@ -9,30 +9,30 @@ using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
+using System.Windows.Forms.Integration;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace LevelEditor
+namespace LevelEditor.UserControls
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for GameHost.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class GameHost : UserControl
     {
+
         public IntPtr MainWindowHandle { get; set; }
 
 
         [DllImport("user32.dll", SetLastError = true)]
         private static extern long SetParent(IntPtr hWndChild, IntPtr hWndNewParent);
 
-
-        public MainWindow()
+        public GameHost()
         {
             InitializeComponent();
-
 
 
             try
@@ -48,9 +48,9 @@ namespace LevelEditor
                 //_Grid.Children.Add(_pnlSched);
 
                 ProcessStartInfo psi = new ProcessStartInfo(System.IO.Directory.GetCurrentDirectory() + @"\..\..\..\Fledermaus\bin\Debug\Fledermaus.exe");
-
-                //psi.CreateNoWindow = false;//.WindowStyle = ProcessWindowStyle.Normal;
-
+          
+                psi.CreateNoWindow = false;//.WindowStyle = ProcessWindowStyle.Normal;
+                
                 Process PR = Process.Start(psi);
 
                 PR.WaitForInputIdle(); // true if the associated process has reached an idle state.
